ec52d9340a5e7bf7c057a19b1efede0526edefe0
MJ Brechner <mjbrechner@gmail.com>
2022-07-17 07:45:37 -0400

    articles now display by date correctly

--- javascript/grab-articles.js
+++ javascript/grab-articles.js
@@ -1,18 +1,31 @@
 'use strict';
 
-// document.getElementById('article-title').textContent = June30.artTitle;
-// document.getElementById('article-subtitle').textContent = June30.artSubtitle;
-// document.getElementById('article-body').textContent = June30.artBody;
-// document.getElementById('article-date').textContent = `${June30.artDay} ${June30.artMonth} 1922`;
-// document.getElementById('article-page').textContent = `p. ${June30.artPage}`;
-
-
 let artDate;
-artDate = monthName+day;
-artDate = July12;
+artDate = `${day} ${monthName}`;
 
-document.getElementById('article-title').textContent = artDate.artTitle;
-document.getElementById('article-subtitle').textContent = artDate.artSubtitle;
-document.getElementById('article-body').textContent = artDate.artBody;
-document.getElementById('article-date').textContent = `${artDate.artDay} ${artDate.artMonth} 1922`;
-document.getElementById('article-page').textContent = `p. ${artDate.artPage}`;
+for (const e of artArray) {
+    if (e.artDateIdentifier === artDate) {
+        document.getElementById('article-title').textContent = e.artContents.artTitle;
+        document.getElementById('article-subtitle').textContent = e.artContents.artSubtitle;
+        document.getElementById('article-body').textContent = e.artContents.artBody;
+        document.getElementById('article-paper').textContent = "Louisville Courier-Journal";
+        document.getElementById('article-date').textContent = `${e.artDateIdentifier} 1922`;
+        document.getElementById('article-page').textContent = `${e.artContents.artPage}`;
+        break;
+    } else if (artDate === "29 February") {
+        document.getElementById('article-title').textContent = "Leap Year!";
+        document.getElementById('article-subtitle').textContent = " ";
+        document.getElementById('article-body').textContent = "Since February only had 28 days in 1922, there is no article to display today. Please check back tomorrow!";
+        document.getElementById('article-paper').textContent = " ";
+        document.getElementById('article-date').textContent = " ";
+        document.getElementById('article-page').textContent = " ";
+        break;
+    } else { // This should never occur but is here  in case of an unexpected error.
+        document.getElementById('article-title').textContent = "Error!";
+        document.getElementById('article-subtitle').textContent = " ";
+        document.getElementById('article-body').textContent = "Oopsie. An error has occurred.";
+        document.getElementById('article-paper').textContent = " ";
+        document.getElementById('article-date').textContent = " ";
+        document.getElementById('article-page').textContent = " ";
+    }
+};
