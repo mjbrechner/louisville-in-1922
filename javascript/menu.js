d10ee2cd3e6d256e242da2bf18fe07163c6bf786
MJ Brechner <mjbrechner@gmail.com>
2022-07-20 16:34:45 -0400

    credits page & work on other pages

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function menuFunction() {
     let e = document.getElementById("nav-menu");
     if (e.style.display === "block") {
@@ -6,4 +7,40 @@ function menuFunction() {
     } else {
         e.style.display = "block"
     }
-}
+};
+
+
+
+// const mediumScreen = window.matchMedia("min-width: 768px");
+// let screenWidth = window.outerWidth;
+
+// function menuAlwaysVisible() {
+// let e = window.outerWidth;
+// if (screenWidth >= 768) {
+// e.style.display = "block";
+// } else {
+//     e.style.display = "none";
+// }
+// }
+
+// menuAlwaysVisible();
+// mediumScreen.addEventListener(menuAlwaysVisible);
+
+// // function menuNeverVisible(smallScreen) {
+// // if (smallScreen.matches) {
+// //     e.style.display = "none"
+// // } else
+// // e.style.display = "block"
+// // };
+// function menuAlwaysVisible(mediumScreen) {
+// if (mediumScreen.matches) {
+//     e.style.display = "none"
+// } else
+// e.style.display = "block"
+// };
+
+
+// // menuNeverVisible(smallScreen);
+// // smallScreen.addEventListener(menuNeverVisible);
+// menuAlwaysVisible(mediumScreen);
+// mediumScreen.addEventListener(menuAlwaysVisible);
